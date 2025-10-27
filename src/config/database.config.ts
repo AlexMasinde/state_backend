5a8a0649b5e7fcede5f35279f529686f0f94ab60
Alex Masinde <daktari2033@gmail.com>
2025-10-28 02:41:13 +0300

    removed sync

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -29,8 +29,8 @@ export const databaseConfig: TypeOrmModuleOptions = {
   database: env.DB_DATABASE,
   autoLoadEntities: true,
   ssl,
-  synchronize: true,
-  // synchronize: env.NODE_ENV !== 'production',
+  // synchronize: true,
+  synchronize: env.NODE_ENV !== 'production',
   logging: env.NODE_ENV === 'development',
 
   // Add connection retry and timeout settings
